3f297986d92ff8826a64d3f1e601bc1fddfc31d8
Chinmay Chandak <chandakchinmay@gmail.com>
2018-02-18 00:54:41 +0530

    testing motor driver in raspberry pi
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ RaspberryPi/motorDriver.py
@@ -0,0 +1,53 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+m1f = 5
+m1r = 6
+m2f = 13
+m2r = 19
+
+GPIO.setup(m1f, GPIO.OUT)
+GPIO.setup(m1r, GPIO.OUT)
+GPIO.setup(m2f, GPIO.OUT)
+GPIO.setup(m2r, GPIO.OUT)
+
+print "starting up motor"
+time.sleep(1)
+
+print "testing motor 1 forward"
+GPIO.output(m1f, True)
+GPIO.output(m1r, False)
+time.sleep(1)
+GPIO.output(m1f, False)
+GPIO.output(m1r, False)
+time.sleep(1)
+
+print "testing motor 1 reverse"
+GPIO.output(m1f, False)
+GPIO.output(m1r, True)
+time.sleep(1)
+GPIO.output(m1f, False)
+GPIO.output(m1r, False)
+time.sleep(1)
+
+#motor 2
+print "testing motor 2 forward"
+GPIO.output(m2f, True)
+GPIO.output(m2r, False)
+time.sleep(1)
+GPIO.output(m2f, False)
+GPIO.output(m2r, False)
+time.sleep(1)
+
+print "testing motor 2 reverse"
+GPIO.output(m2f, False)
+GPIO.output(m2r, True)
+time.sleep(1)
+GPIO.output(m2f, False)
+GPIO.output(m2r, False)
+time.sleep(1)
+
+GPIO.cleanup()
